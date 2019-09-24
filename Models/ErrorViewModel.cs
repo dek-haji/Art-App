8035c132fccc5ad77f85d1639971e5539591e731
dek-haji <deeeqhaji@gmail.com>
2019-09-23 21:56:43 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Art_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
